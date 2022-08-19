6b244bac8752002b88160fb1a14785714fe37c8c
TravlingCoder <travlingcoder@gmail.com>
2022-08-18 21:37:25 -0600

    try timeout

--- index.js
+++ index.js
@@ -48,10 +48,10 @@ const tcgPlayerDownloadPrinter = async () => {
     await downloadAllOrderPDFs(page);
     setTimeout(async () => {
       await printAllOrders();
-    }, orders.length * 1000); 
+    }, orders.length * 2000); 
     setTimeout(async () => {
       await moveAllFiles();
-    }, orders.length * 3000);
+    }, orders.length * 6000);
 
     await browser.close();
   }catch(err){
