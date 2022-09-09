ec04673719fbf2c0f3c7db8a34cc08c12889f23c
TheTravlingCoder <travlingcoder@gmail.com>
2022-09-09 10:14:44 -0600

    added server for cron functionality

---
+++ server.js
@@ -0,0 +1,16 @@
+// create basic express server
+const express = require('express');
+const cron = require('node-cron')
+const app = express();
+const server = require('http').createServer(app);
+const port = process.env.PORT || 3000;
+
+server.listen(port, function () {
+  console.log('Server listening at port %d', port);
+});
+
+// create cron job that runs index.js every hour
+cron.schedule('0 * * * *', () => {
+  console.log('running a task every hour');
+  require('./index.js');
+}, null, true, 'America/Los_Angeles');
